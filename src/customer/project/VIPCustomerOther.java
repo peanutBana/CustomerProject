afc396aff6464e4e79d4e0bb7364711ed41c83d9
peanutBana <diao0321@gmail.com>
2022-11-24 17:46:18 +0900

    'upload'

---
+++ src/customer/project/VIPCustomerOther.java
@@ -0,0 +1,42 @@
+package customer.project;
+
+
+public class VIPCustomerOther extends Customer {
+	private int agentID;
+	double saleRatio;
+	
+	public VIPCustomerOther(int customerID, String customerName, int agentID){
+		super(customerID, customerName);
+	
+		super.customerGrade = "VIP";
+		super.bonusRatio = 0.03;
+		this.saleRatio = 0.2;
+		this.agentID = agentID;
+	}
+	
+	/*
+	【문항3】 아래 항목과 클래스 다이어그램을 참고하여 Customer 클래스에서 override된 calcPrice 메소드를 
+        VIPCustomer.java에 작성하시오.
+         - 보너스 포인트 계산법은 동일하다.
+         - price는 리턴시킨다. *공식:  price - (price * saleRatio)  
+    */
+	@Override
+	public int calcPrice(int price){
+		bonusPoint += (price * bonusRatio); //보너스 포인트 계산
+		return price; 
+	}
+	/*
+	【문항4】 아래 항목과 클래스 다이어그램을 참고하여 Customer 클래스에서 override된 showCustomerInfo
+    메소드를 VIPCustomer.java에 작성하시오.
+      -메소드 실행시 실행 결과는 ‘이율곡님의 등급: VIP, 보너스 포인트: 0점, 담당 상담원 번호:12345’.
+      - Customer클래스의 showCustomerInfo()를 호출하여 완성.
+     */
+		@Override
+		public String showCustomerInfo(){
+			return customerName + "님의 등급: " + customerGrade + ", 보너스 포인트: " + bonusPoint + "점, 담당 상담원 번호 : " +  agentID ;  
+		}
+
+	public int getAgentID(){
+		return agentID;
+	}
+}
